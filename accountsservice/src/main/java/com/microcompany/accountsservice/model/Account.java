2bdb97c606243c27a8246dc9820533bb3d37bf5b
alex55fc <alexander.55.fc@gmail.com>
2025-03-19 13:54:12 +0100

    Esqueleto

---
+++ accountsservice/src/main/java/com/microcompany/accountsservice/model/Account.java
@@ -0,0 +1,37 @@
+package com.microcompany.accountsservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+
+    @DateTimeFormat
+    Date openingDate;
+
+    private int balance;
+
+    private Long ownerId;
+
+    @Transient
+    Customer owner;
+
+
+}
