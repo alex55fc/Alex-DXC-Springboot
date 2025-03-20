a21e8f28fe677f2d3cb7ca881afc6041be546e92
alex55fc <alexander.55.fc@gmail.com>
2025-03-20 12:58:47 +0100

    Account getter & setters. Constructors added

--- accountsservice/src/main/java/com/microcompany/accountsservice/model/Account.java
+++ accountsservice/src/main/java/com/microcompany/accountsservice/model/Account.java
@@ -36,4 +36,54 @@ public class Account {
     @JoinColumn(name = "owner_id", referencedColumnName = "id")
     Customer owner;
 
+    public Account() {
+    }
+
+    public Account(Long id, String type, Date openingDate, int balance, Customer owner) {
+        this.id = id;
+        this.type = type;
+        this.openingDate = openingDate;
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+
+    public Date getOpeningDate() {
+        return openingDate;
+    }
+
+    public void setOpeningDate(Date openingDate) {
+        this.openingDate = openingDate;
+    }
 }
