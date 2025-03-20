cdda3a3b5218c69fde6a38f8075fbc4f53a9a4af
adriansotte <adrianmvparedes@gmail.com>
2025-03-20 10:28:00 +0100

    Cambiado nombres

--- accountsservice/src/main/java/com/microcompany/accountsservice/AccountsserviceApplication.java
+++ accountsservice/src/main/java/com/microcompany/accountsservice/AccountsserviceApplication.java
@@ -2,8 +2,10 @@ package com.microcompany.accountsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.microcompany.accountsservice.model")
 public class AccountsserviceApplication {
 
 	public static void main(String[] args) {
